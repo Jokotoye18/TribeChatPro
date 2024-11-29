682d2b8dc0829c6272bf13e5c0b3e63df3f289e4
Jokotoye Ademola <jokotoyeademola95@gmail.com>
2024-11-29 12:28:51 +0100

    feat:  setup chat background constant

---
+++ src/screen/ChatRoom/constants/chatBackgroundUri.ts
@@ -0,0 +1,6 @@
+export const uri = [
+  require("../../../../assets/images/chat-background.jpg"),
+  require("../../../../assets/images/chat-background-2.jpg"),
+  require("../../../../assets/images/chat-background-3.jpg"),
+  require("../../../../assets/images/chat-background-4.jpg"),
+];
